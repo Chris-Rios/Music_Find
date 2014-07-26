cd68de6442963b927c2d56adb8d60a9a1b9fa3c2
Chris-Rios <rioschm@gmail.com>
2014-07-26 12:12:19 -0400

    Added recursive search through specified search folder and its subfolders

--- Music_Find.py
+++ Music_Find.py
@@ -6,10 +6,28 @@ import re
 import os
 import shutil
 
+
+def folder_crawler(dir_name,file_type):
+    '''Explores all folders and sub folders for the speicified file type'''
+    paths = os.listdir(dir_name)
+    print(paths)
+    for path in paths:
+        if path.endswith(file_type):
+            print("found an mp3 called %s in %s" %(path,dir_name))
+        elif os.path.isdir(os.path.join(dir_name,path)) and not path.startswith("."):
+            print("found a directory %s" %path)
+            folder_crawler(os.path.join(dir_name,path),file_type)
+            print("finished with %s" %path)
+        else:
+            print("%s is not an %s or a directory" %(path,file_type))    
+
+
+
+
 def main():
     file_type= input('Please enter file type,"mp3","."flac" or "wav", or "all"\n')
-    if (not file_type=='.mp3' and not file_type=='.flac'
-        and not file_type=='.wav' and not file_type=='all'):
+    if (not file_type=='mp3' and not file_type=='flac'
+        and not file_type=='wav' and not file_type=='all'):
     	print ("%s is not a proper option" % file_type)
     	sys.exit(1)
     
@@ -27,6 +45,10 @@ def main():
         else:
             os.mkdir(destination_folder)
             print('folder %s created' %destination_folder)
+            folder_crawler(source_folder,file_type)
+    else:
+        folder_crawler(source_folder,file_type)
+
 
 if __name__ == "__main__":
   main()
